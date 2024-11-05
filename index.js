6cc97fd0067c8375efa6ad8f458f1fd0f9d2314f
azizakii <eleusizovaaziza820@gmail.com>
2024-11-05 11:57:21 +0300

    add new features

--- index.js
+++ index.js
@@ -1,35 +1,91 @@
-const LIMIT = 10000;
 const CURRENCY = 'руб.';
 const STATUS_IN_LIMIT = 'все хорошо';
 const STATUS_OUT_OF_LIMIT = 'все плохо';
 const STATUS_OUT_OF_LIMIT_CLASSNAME = 'status_red';
+const STORAGE_LABEL_LIMIT = 'limit';
+const STORAGE_LABEL_EXPENSES = 'expenses';
 
 const inputNode = document.querySelector('.js-input');
 const buttonNode = document .querySelector('.js-add-button');
 const historyNode = document.querySelector('.js-history');
 const sumNode = document.querySelector('.js-sum');
 const limitNode = document.querySelector('.js-limit');
-const statusNode = document.querySelector('.js-status')
+const statusNode = document.querySelector('.js-status');
+const resetButton = document.querySelector('.js-reset-btn');
+const categorySelect = document.querySelector('.js-categorySelect');
+const changeLimitButton = document.querySelector('.js-changeLimitButton')
 
-const expenses = [];
+let limit = 10000;
+
+function initLimit() {
+    const limitFromStorage = parseInt(localStorage.getItem(STORAGE_LABEL_LIMIT))
+    if (!limitFromStorage) {
+        return;
+    }
+    limit = limitFromStorage;
+    limitNode.innerText = limit;
+}
+
+initLimit();
+
+const expensesFromStorage = localStorage.getItem(STORAGE_LABEL_EXPENSES);
+const expensesFromUser = JSON.parse(expensesFromStorage)
+let expenses = [];
+
+if(Array.isArray(expensesFromUser)) {
+    expenses = expensesFromUser;
+}
+render(expenses);
 
 init(expenses);
 
 buttonNode.addEventListener('click', function() {
    const expense = getExpensesFromUser();
+   const currentCategory = getCategoryFromUser();
+
+   if (currentCategory === 'Категория'){
+    alert ('Выберите категорию')
+        return;
+   }
     
    if (!expense) {
         return;
    }
 
-   trackExpenses(expense);
+   const newExpenses = {amount: expense, category: currentCategory}
+   console.log(newExpenses)
+
+   trackExpenses(newExpenses);
+   saveExpensesToStorage();
 
    render(expenses);
 
+   clearInput(inputNode);
+})
+
+resetButton.addEventListener('click', function() {
+    expenses = [];
+    render(expenses);
+})
+
+changeLimitButton.addEventListener('click', function() {
+    const newLimit = prompt('Новый лимит');
+
+    const newLimitValue = parseInt(newLimit);
+
+    if(!newLimitValue) {
+        return;
+    }
+
+    limitNode.innerText = newLimitValue;
+    limit = newLimitValue;
+    localStorage.setItem(STORAGE_LABEL_LIMIT, newLimitValue)
+
+    render(expenses);
 })
 
 function init(expenses) {
-    limitNode.innerText = LIMIT;
+    limitNode.innerText = limit;
     statusNode.innerText = STATUS_IN_LIMIT;
     sumNode.innerText = calculateExpenses(expenses);
 }
@@ -45,20 +101,29 @@ function getExpensesFromUser() {
 
     const expense = parseInt(inputNode.value);
 
-    clearInput();
-
     return expense;
 }
 
-function clearInput() {
-    inputNode.value = '';
+function getCategoryFromUser() {
+    const category = categorySelect.value;
+
+    return category;
+}
+
+function clearInput(input) {
+    input.value = '';
+}
+
+function saveExpensesToStorage() {
+    const expensesString = JSON.stringify(expenses);
+    localStorage.setItem(STORAGE_LABEL_EXPENSES, expensesString);
 }
 
 function calculateExpenses(expenses) {
     let sum = 0;
 
     expenses.forEach(element => {
-        sum += element;
+        sum += element.amount;
     });
 
     return sum;
@@ -68,16 +133,16 @@ function calculateExpenses(expenses) {
 function render(expenses) {
     const sum = calculateExpenses(expenses);
 
-    renderHistory(expenses);
+    renderHistory();
     renderSum(sum);
     renderStatus(sum);
 }
 
-function renderHistory(expenses) {
+function renderHistory() {
     let expensesHTML = '';
     
     expenses.forEach(element => {
-        const elementHtml = `<li class="history-item">${element}${CURRENCY}</li>`;
+        const elementHtml = `<li class="history-item">${element.category} - ${element.amount}${CURRENCY}</li>`;
         expensesHTML += elementHtml;
     });
     
@@ -89,10 +154,12 @@ function renderSum(sum) {
 }
 
 function renderStatus(sum) {
-    if (sum <= LIMIT) {
+    if (sum <= limit) {
         statusNode.innerText = STATUS_IN_LIMIT;
+        statusNode.classList.remove(STATUS_OUT_OF_LIMIT_CLASSNAME)
     } else {
-        statusNode.innerText = STATUS_OUT_OF_LIMIT;
+        statusNode.innerText = `${STATUS_OUT_OF_LIMIT} (${limit - sum} руб.)`;
         statusNode.classList.add(STATUS_OUT_OF_LIMIT_CLASSNAME)
     }
 }
+
